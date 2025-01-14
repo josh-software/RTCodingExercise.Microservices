50fa9a90cee55f75a8bf443ad1484dfff65a59e8
Joshua Skidmore <81509399+josh-software@users.noreply.github.com>
2025-01-14 16:01:11 +0000

    Simplified state management for Home

--- src/Web/WebMVC/Models/HomeViewModel.cs
+++ src/Web/WebMVC/Models/HomeViewModel.cs
@@ -1,7 +1,11 @@
 ﻿namespace WebMVC.Models
 {
-    public class HomeViewModel<T> : PaginationStateModel
+    public class HomeViewModel<T>
     {
+        public PaginationStateModel PaginationState { get; set; } = new PaginationStateModel();
+
+        public int TotalPages { get; set; }
+
         public IEnumerable<T> Items { get; set; } = new List<T>();
     }
 }
--- src/Web/WebMVC/Models/PaginationStateModel.cs
+++ src/Web/WebMVC/Models/PaginationStateModel.cs
@@ -5,6 +5,39 @@
         public string SortBy { get; set; } = "Id";
         public string SortDirection { get; set; } = "asc";
         public int CurrentPage { get; set; } = 1;
-        public int TotalPages { get; set; }
+
+        public PaginationStateModel ChangeSortBy(string sortBy)
+        {
+            var newModel = Clone();
+
+            if (newModel.SortBy == sortBy)
+            {
+                newModel.SortDirection = newModel.SortDirection == "asc" ? "desc" : "asc";
+            }
+            else
+            {
+                newModel.SortBy = sortBy;
+                newModel.SortDirection = "asc";
+            }
+
+            return newModel;
+        }
+
+        public PaginationStateModel ChangePage(int pageNumber)
+        {
+            var newModel = Clone();
+            newModel.CurrentPage = pageNumber;
+            return newModel;
+        }
+
+        private PaginationStateModel Clone()
+        {
+            return new PaginationStateModel()
+            {
+                SortBy = SortBy,
+                SortDirection = SortDirection,
+                CurrentPage = CurrentPage
+            };
+        }
     }
 }
